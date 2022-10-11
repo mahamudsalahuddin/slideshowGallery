51e035e698cfedecc36657e53de043ef65dbe2f2
Salahuddin Mahmud <76897399+s-mahmud52@users.noreply.github.com>
2022-10-11 08:52:53 +0600

    dots slider

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ function currentSlide(n){
 function showSlides(n){
     let slides = document.querySelectorAll(".mySlides");
     let dots = document.querySelectorAll(".demo");
+    let slidesdot = document.querySelectorAll(".dot");
     let captionText = document.querySelectorAll("#caption");
     if(n > slides.length){
         slideIndex = 1;
@@ -18,13 +19,18 @@ function showSlides(n){
     }
     let slidesArr = Array.from(slides);
     let dotsArr = Array.from(dots);
+    let slidesdotsArr = Array.from(slidesdot);
     slidesArr.map((item)=>{
         item.style.display = "none";
     });
     dotsArr.map((item)=>{
         item.className = item.className.replace(" active", "");
     });
+    slidesdotsArr.map((item)=>{
+        item.className = item.className.replace(" active", "");
+    });
     slides[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className += " active";
+    slidesdot[slideIndex-1].className += " active";
     captionText.innerHTML = dots[slideIndex-1].lastChild;
 }
